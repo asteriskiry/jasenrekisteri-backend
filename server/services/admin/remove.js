c6720971e675cdcb7e1f581dd0765dba2678aae5
mjturt <maks.turtiainen@gmail.com>
2019-05-23 20:30:24 +0300

    Remove api

--- server/services/admin/remove.js
+++ server/services/admin/remove.js
@@ -4,11 +4,11 @@ const utils = require('../../utils');
 const httpResponses = require('./');
 
 function remove(request, response) {
-    const accessTo = request.query.access.toLowerCase();
+    const accessTo = request.body.access.toLowerCase();
 
     if (accessTo === 'admin' || accessTo === 'board') {
-        utils.checkUserControl(request.body.admin.id).then(admin => {
-            Member.remove({ _id: request.body.id }, function(err) {
+        utils.checkUserControl(request.body.id).then(admin => {
+            Member.remove({ _id: request.body.memberID }, function(err) {
                 if (err) response.json(err);
                 response.json({ success: true, message: 'Jäsen poistettu.' });
             });
