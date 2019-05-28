e6313aa6b70361170034837dcc36fb175a411518
mjturt <maks.turtiainen@gmail.com>
2019-05-28 19:49:06 +0300

    Production email settings

--- config/mail.js
+++ config/mail.js
@@ -6,22 +6,21 @@ const nodemailer = require('nodemailer');
 const boardMailAddress = config.boardMailAddress;
 const mailSender = config.mailSender;
 const useGmail = config.useGmail;
-let transporter;
 
-if (useGmail) {
-    transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: config.gmailUser,
-            pass: config.gmailPassword,
-        },
-    });
-} else {
-    transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail',
-    });
-}
+const gmailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.gmailUser,
+        pass: config.gmailPassword,
+    },
+});
+
+const sendmailTransporter = nodemailer.createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/sbin/sendmail',
+});
+
+const transporter = useGmail === '1' ? gmailTransporter : sendmailTransporter;
 
 module.exports = { boardMailAddress, mailSender, transporter };
