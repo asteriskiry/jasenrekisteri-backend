4c66590d96d81348322d08b1727a2afa1b2ab932
mjturt <maks.turtiainen@gmail.com>
2019-10-31 21:34:43 +0200

    Mailing script added

---
+++ scripts/mass-mailer.js
@@ -0,0 +1,51 @@
+// Script for sending mail for all members
+
+const mongoose = require('mongoose');
+const Member = require('../server/models/Member');
+const mail = require('../config/mail');
+const config = require('../config/config');
+const args = require('minimist')(process.argv.slice(2), {
+    alias: {
+        h: 'help',
+        s: 'subject',
+        e: 'email',
+    },
+});
+
+function help() {
+    console.log('Script for sending mail for all members');
+    console.log('Usage: npm run mass-mailer -- -s <subject> -e <path to file that contains email body>');
+    console.log('-h for help');
+}
+
+if (args.h) {
+    help();
+    process.exit();
+} else if (!args.s || !args.e) {
+    help();
+    process.exit();
+}
+
+let subject = args.s;
+let emailFilePath = args.e;
+
+mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+
+db.once('open', async function() {
+    await Member.find({}, function(err, members) {
+        if (err) console.log(err);
+        members.map(user => {
+            let mailOptions = {
+                from: mail.mailSender,
+                to: user.email,
+                subject: subject,
+                text: { path: emailFilePath },
+            };
+            mail.transporter.sendMail(mailOptions);
+        });
+    });
+    db.close(() => {
+        console.log(('Mails sent'));
+    });
+});
